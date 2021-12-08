fdea729ffd188d92f769202fd10224e98c6706da
KEVIN ALVES DA SILVA <21193393@CFP127ALUNO.LOCAL>
2021-12-08 15:37:45 -0300

    Configurando API para poder ser consumida

--- API/index.php
+++ API/index.php
@@ -1,8 +1,14 @@
 <?php
-   
-   $url = (string) null;
-   
-   $url = explode('/', $_GET['url']);
+
+    // Permissões e configurações para a API responder em um servidor real
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Header: Content-Type');
+    header('Content-Type: application/json');
+    
+    $url = (string) null;
+
+    $url = explode('/', $_GET['url']);
 
     switch ($url[0]) {
         
